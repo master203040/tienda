8d0f7d88c2dd7ed38e7664d61b6021c9df0252aa
carlos ortega <carlosalbertoortegac@gmail.com>
2021-10-21 21:19:40 -0500

    se realiza la concexion a la bd

--- models/BaseDatos.php
+++ models/BaseDatos.php
@@ -1,56 +1,24 @@
 <?php
 
-class BaseDatos{
+    $conn=mysqli_connect(
 
-    public $usuario = "root";
-    public $passworldDB = "";
-    public $servidorDB = "mysgl:host = localhost;";
-    public $nombreDB = "dbname = theshop";
+        'localhost',
+        'root',
+        '',
+        'tienda'
 
-    // metodos especial constructor
-    public function __construct(){}
+    );
 
-    //metodo = accion = funciones 
-    public function conectarConBD(){
+    //if (isset($conn)){
 
+        //echo("exito");
+   // }
 
-        try {
-
-            $datosGeneralesBD = $this-> $servidorDB.$this->nombreDB;
-            $conexion = new PDO( $datosGeneralesBD,$this->usuario,$this->passworldDB);
-            echo("exit connection a DB");
+    
 
 
-        } catch (PDOException  $error) {
-            
-            die("problema en el servidor ".$error ->getMessage());
-        }
 
-    }
 
-    public function insertarRegistro($consultaSQL){
-
-        try {
-            
-            //1.construir una puente con la BD
-            $conexion = $this->conectarConBD();
-
-            //2.prepara BD para que reciba mi consulta
-            $operacion =$conexion->prepare($consultaSQL);
-
-            //3.ejecutar la operacion 
-            $resultado = $operacion ->execute();
-
-            return $resutado;
-
-        } catch () {
-            
-            die ("problema en el servidor ".$error ->getMessage()")
-        }
-        
-    }
-
-}
 
 
 
